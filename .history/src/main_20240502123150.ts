f0687c71c4ede6ca98ea70783580d245e56b9a30
Jonathan Gold <jgold@instate.io>
2024-05-02 13:54:07 +0300

    Ver 0.1.1: Updated config

--- .history/src/main_20240502123150.ts
+++ .history/src/main_20240502123150.ts
@@ -10,4 +10,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
