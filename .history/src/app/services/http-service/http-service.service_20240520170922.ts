17bb9aa9fc8710e0fb08350ee05abdde70eb679e
Jonathan Gold <jgold@instate.io>
2024-05-21 09:19:32 +0300

    Ver 0.1.2

---
+++ .history/src/app/services/http-service/http-service.service_20240520170922.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+
+
+
+@Injectable({ providedIn: 'root' })
+export class HttpClientService {
+
+
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(
+    private http: HttpClient,
+    ) { }
+
+    public get(url: string, options?: RequestOptionsArgs, silent?: boolean): Observable<any> {
+      return this.createRequest(RequestMethod.Get, url, undefined, options, silent);
+    }
+  
+    public post(url: string, body: any, options?: RequestOptionsArgs, silent?: boolean): Observable<any> {
+      return this.createRequest(RequestMethod.Post, url, body, options, silent);
+    }
+  
+    public put(url: string, body: any, options?: RequestOptionsArgs): Observable<any> {
+      return this.createRequest(RequestMethod.Put, url, body, options);
+    }
+  
+    public delete(url: string, options?: RequestOptionsArgs): Observable<any> {
+      return this.createRequest(RequestMethod.Delete, url, undefined, options);
+    }
+  
+
+
+
+
+}
---
+++ .history/src/app/services/http-service/http-service.service_20240520173928.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+
+
+
+@Injectable({ providedIn: 'root' })
+export class HttpClientService {
+
+
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(
+    private http: HttpClient,
+    ) { }
+
+   
+ getServiceData(url: any): Observable<any> {
+    return this.http.get<any>(url);
+  }
+
+  postServiceData(url: any, body:any): Observable<any> {
+    return this.http.post<any>(url, body, this.httpOptions);
+  }
+  
+
+
+
+
+}
