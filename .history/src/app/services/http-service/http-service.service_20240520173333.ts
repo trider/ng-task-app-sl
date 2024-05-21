1018320d4bd817a4e5b395daefdb5faf89bb2ffa
Jonathan Gold <jgold@instate.io>
2024-05-21 09:19:32 +0300

    Ver 0.1.2

---
+++ .history/src/app/services/http-service/http-service.service_20240520173333.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClientModule, HttpHeaders } from '@angular/common/http';
+
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+
+
+
+@Injectable({ providedIn: 'root' })
+export class HttpClientService {
+
+
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(
+
+    ) { }
+
+   
+  public getServiceData(url: any): Observable<any> {
+    return this.http.get<any>(url);
+  }
+
+  public postServiceData(url: any, body:any): Observable<any> {
+    return this.http.post<any>(url, body, this.httpOptions);
+  }
+  
+
+
+
+
+}
